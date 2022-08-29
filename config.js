2faa7870778729c0ca2570f8b2a36ec206fef201
genhack <agliarulo@hotmail.it>
2022-08-29 14:37:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const endpointHttp =
   process.env.NODE_ENV === 'production'
-    ? 'https://exam-maker-backend.herokuapp.com/graphql'
+    ? 'https://exam-maker-backend-2.herokuapp.com/graphql'
     : 'http://localhost:8081/graphql'
 export const frontend =
   process.env.NODE_ENV === 'production'
-    ? 'https://exam-maker.herokuapp.com'
+    ? 'https://exam-maker-2.herokuapp.com'
     : 'http://localhost:8080'
 export const googleClientID =
   '162812165284-hvi6iqeqtatt48d2cv824d3bl4n9b5ig.apps.googleusercontent.com'
